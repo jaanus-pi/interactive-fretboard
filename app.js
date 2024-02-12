5987995849e3358f9a09fb9d7cc663dff1e930b4
jaanus-pi <jaanus-soft@proton.me>
2024-02-12 14:18:22 +0200

    dynamic fretmarks

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const fretboard = document.querySelector('.fretboard');
 const numberOfFrets = 12;
 const numberOfStrings = 6;
 
+const singleFretMarkPositions = [3, 5, 7, 9, 15, 17, 19, 21];
+const doubleFretMarkPositions = [12, 24];
+
 const app = {
   init() {
     this.setupFretboard();
@@ -18,6 +21,16 @@ const app = {
         let noteFret = tools.createElement('div');
         noteFret.classList.add('note-fret');
         string.appendChild(noteFret);
+        // Add single fretmarks
+        if (i === 0 && singleFretMarkPositions.indexOf(fret) !== -1) {
+          noteFret.classList.add('single-fretmark')
+        }
+        // Add double fretmarks
+        if (i === 0 && doubleFretMarkPositions.indexOf(fret) !== -1) {
+          let doubleFretMark = tools.createElement('div');
+          doubleFretMark.classList.add('double-fretmark');
+          noteFret.appendChild(doubleFretMark);
+        }
       }
     }
   }
