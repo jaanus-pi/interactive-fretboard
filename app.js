972688b3e32844c09423a771a9e2bb23f62b0041
jaanus-pi <jaanus-soft@proton.me>
2024-02-13 15:02:57 +0200

    notes on the fretboard

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const numberOfStrings = 6;
 const singleFretMarkPositions = [3, 5, 7, 9, 15, 17, 19, 21];
 const doubleFretMarkPositions = [12, 24];
 
+const notesFlat = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"];
+const notesSharp = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+
+let accidentals = 'sharps';
+const guitarTuning = [4, 11, 7, 2, 9, 4];
+
 const app = {
   init() {
     this.setupFretboard();
@@ -24,6 +30,10 @@ const app = {
         let noteFret = tools.createElement('div');
         noteFret.classList.add('note-fret');
         string.appendChild(noteFret);
+
+        let noteName = this.generateNoteNames((fret + guitarTuning[i]));
+        noteFret.setAttribute('data-note', noteName);
+
         // Add single fretmarks
         if (i === 0 && singleFretMarkPositions.indexOf(fret) !== -1) {
           noteFret.classList.add('single-fretmark')
@@ -36,6 +46,16 @@ const app = {
         }
       }
     }
+  },
+  generateNoteNames(noteIndex) {
+    noteIndex = noteIndex % 12;
+    let noteName;
+    if (accidentals === 'flats') {
+      noteName = notesFlat[noteIndex];
+    } else if (accidentals === 'sharps') {
+      noteName = notesSharp[noteIndex];
+    }
+    return noteName;
   }
 }
 
